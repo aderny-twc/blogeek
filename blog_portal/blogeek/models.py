8d548e0e66a5614cc93569d46c55514ae88f4764
Andrey Belskiy <diksandab@gmail.com>
2021-02-03 08:51:25 +0600

    Created models for application 'blogeek'; added static urls

--- blog_portal/blogeek/models.py
+++ blog_portal/blogeek/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BlogPost(models.Model):
+    """
+    Blog article class.
+    """
+    title = models.CharField(max_length=150, verbose_name='Название')
+    description = models.CharField(max_length=600, verbose_name='Описание')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views = models.IntegerField(default=0)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """
+    Article category class.
+    """
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')
+    description = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
