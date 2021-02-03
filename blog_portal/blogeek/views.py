874ace642f1d66674ebe0a436d6b614e7e4aae94
Andrey Belskiy <diksandab@gmail.com>
2021-02-03 11:59:14 +0600

    Added display of articles by category

--- blog_portal/blogeek/views.py
+++ blog_portal/blogeek/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import BlogPosts
+from .models import BlogPosts, Category
 
 
 def index(request):
     posts = BlogPosts.objects.order_by('-created_at')
+    categories = Category.objects.all()
     title = 'Список статей'
-    return render(request, 'blogeek/index.html', {'posts': posts, 'title': title})
+    context = {
+        'posts': posts,
+        'title': title,
+        'categories': categories,
+    }
+    return render(request, 'blogeek/index.html', context)
+
+
+def get_category(request, category_id):
+    posts = BlogPosts.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'posts': posts,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request, 'blogeek/category.html', context)
